de9fe038e1af5cc60673f58ce927736c9d86c435
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-15 00:12:34 -0800

    Import Carousel from react-bootstrap. Add Carousel tag and subtags. Remove template values for src and alt attributes for img tags.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import HappyBirthday from "./components/HappyBirthday";
+import { Carousel } from 'react-bootstrap';
 
 function App() {
   return (
@@ -7,6 +8,44 @@ function App() {
       {/* Falling Confetti */}
       <div className="container">
         <h1 className="title-text">Joyeux 30e Anniversaire, Andréa!</h1>
+
+        <Carousel>
+          <Carousel.Item interval={1000}>
+            <img
+              className="d-block w-100"
+              src=""
+              alt=""
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <img
+              className="d-block w-100"
+              src=""
+              alt=""
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src=""
+              alt=""
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
         <div className="confetti"></div>
         <div className="confetti"></div>
         <div className="confetti"></div>
