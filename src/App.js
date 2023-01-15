4c8fd929fed0a00b89ff52da18cfcd1d82194e3c
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-14 23:42:45 -0800

    Add falling confetti with modified background colour, taken from Codepen.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,56 @@ import HappyBirthday from "./components/HappyBirthday";
 function App() {
   return (
     <div>
+      {/* Falling Confetti */}
+      <div class="container">
+        <p class="nav-text">Joyeux 30e Anniversaire, Andréa!</p>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+      </div>
+
       <HappyBirthday />
+
     </div>
   );
 }
