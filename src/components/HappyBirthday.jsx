011a8f41938e933adf07bc9cb911f19e9720ef42
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-16 23:54:51 -0800

    Copy & paste Carousel component and contents from App.js - clean up App.js.

--- src/components/HappyBirthday.jsx
+++ src/components/HappyBirthday.jsx
@@ -6,7 +6,39 @@ import { Carousel } from "react-bootstrap";
 export default function HappyBirthday() {
   return (
     <div>
-
+      <Carousel className="carousel">
+        <Carousel.Item interval={4000}>
+          <img
+            className="d-block w-100"
+            src={picOne}
+            alt="Golden retriever puppies"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={4000}>
+          <img className="d-block w-100" src={picTwo} alt="Aussie puppy" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={picThree}
+            alt="Daschund mix puppy"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
