c301264cb18b36e6925cc251f6d04bd6402777d0
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-17 23:18:44 -0800

    Import rest of photos to be used in Carousel.

--- src/components/HappyBirthday.jsx
+++ src/components/HappyBirthday.jsx
@@ -4,7 +4,18 @@ import { Carousel } from "react-bootstrap";
 import whistlerPic from "../assets/whistler2018.jpg";
 import bcLionsPic from "../assets/bclionsgame2019.jpg";
 import panoRidgePic from "../assets/panoridge2019.JPG";
-
+import xmasPartyPic1 from "../assets/xmasparty2019-1.JPG";
+import xmasPartyPic2 from "../assets/xmasparty2019-2.JPG";
+import mtSeymourPic from "../assets/seymour2020.jpg";
+import howeSoundPic1 from "../assets/howesound2021-1.jpg";
+import howeSoundPic2 from "../assets/howesound2021-2.JPG";
+import canucksGamePic from "../assets/canucksgame2021.JPG";
+import joffreLakePic from "../assets/joffrelake2022.jpg";
+import sunshineCoastPic1 from "../assets/sunshinecoast2022-1.jpg";
+import sunshineCoastPic2 from "../assets/sunshinecoast2022-2.JPG";
+import iceBergLakePic1 from "../assets/iceberglake2022-1.JPG";
+import iceBergLakePic2 from "../assets/iceberglake2022-2.jpg";
+import mtSproatt2022 from "../assets/mtsproatt2022.jpg";
 
 export default function HappyBirthday() {
 
@@ -18,14 +29,14 @@ export default function HappyBirthday() {
     {
       id: 2,
       photoName: bcLionsPic,
-      altValue: "BC Lions Game 2019"
+      altValue: "BC Lions Game 2019",
     },
     {
       id: 3,
       photoName: panoRidgePic,
-      altValue: "Panorama Ridge Hike & Camp 2019"
-    }
-  ]
+      altValue: "Panorama Ridge Hike & Camp 2019",
+    },
+  ];
 
   return (
     <div>
