54b5c17a20ab81a1520f92303a3a13b4bf646439
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-17 20:59:48 -0800

    Remove commented out Carousel component used previously.

--- src/components/HappyBirthday.jsx
+++ src/components/HappyBirthday.jsx
@@ -29,43 +29,6 @@ export default function HappyBirthday() {
 
   return (
     <div>
-      {/* <Carousel className="carousel">
-        <Carousel.Item interval={4000}>
-          <img
-            className="d-block w-100"
-            src={whistlerPic}
-            alt="Whistler Village 2018"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={4000}>
-          <img
-            className="d-block w-100"
-            src={bcLionsPic}
-            alt="BC Lions Game 2019"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={panoRidgePic}
-            alt="Panorama Ridge August 2019"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel> */}
       <Carousel className="carousel">
         {photoAlbum.map((photo) => {
           return (
