519569cb7a0c4722ed4032bfec776af8a6fc6177
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-17 23:33:27 -0800

    Remove import for one of the Xmas party photos. Rename iceberg lake variable name in import line. Add rest of imported photos into photoAlbum array.

--- src/components/HappyBirthday.jsx
+++ src/components/HappyBirthday.jsx
@@ -4,8 +4,7 @@ import { Carousel } from "react-bootstrap";
 import whistlerPic from "../assets/whistler2018.jpg";
 import bcLionsPic from "../assets/bclionsgame2019.jpg";
 import panoRidgePic from "../assets/panoridge2019.JPG";
-import xmasPartyPic1 from "../assets/xmasparty2019-1.JPG";
-import xmasPartyPic2 from "../assets/xmasparty2019-2.JPG";
+import xmasPartyPic from "../assets/xmasparty2019-2.JPG";
 import mtSeymourPic from "../assets/seymour2020.jpg";
 import howeSoundPic1 from "../assets/howesound2021-1.jpg";
 import howeSoundPic2 from "../assets/howesound2021-2.JPG";
@@ -13,8 +12,8 @@ import canucksGamePic from "../assets/canucksgame2021.JPG";
 import joffreLakePic from "../assets/joffrelake2022.jpg";
 import sunshineCoastPic1 from "../assets/sunshinecoast2022-1.jpg";
 import sunshineCoastPic2 from "../assets/sunshinecoast2022-2.JPG";
-import iceBergLakePic1 from "../assets/iceberglake2022-1.JPG";
-import iceBergLakePic2 from "../assets/iceberglake2022-2.jpg";
+import icebergLakePic1 from "../assets/iceberglake2022-1.JPG";
+import icebergLakePic2 from "../assets/iceberglake2022-2.jpg";
 import mtSproatt2022 from "../assets/mtsproatt2022.jpg";
 
 export default function HappyBirthday() {
@@ -35,6 +34,61 @@ export default function HappyBirthday() {
       id: 3,
       photoName: panoRidgePic,
       altValue: "Panorama Ridge Hike & Camp 2019",
+    },
+    {
+      id: 4,
+      photoName: xmasPartyPic,
+      altValue: "Christmas Party 2019",
+    },
+    {
+      id: 5,
+      photoName: mtSeymourPic,
+      altValue: "Pump Peak Hike 2020",
+    },
+    {
+      id: 6,
+      photoName: howeSoundPic1,
+      altValue: "Howe Sound Crest Trail, behind the Lions",
+    },
+    {
+      id: 7,
+      photoName: howeSoundPic2,
+      altValue: "Howe Sound Crest Trail, in the clouds",
+    },
+    {
+      id: 8,
+      photoName: canucksGamePic,
+      altValue: "Canucks Game Dec 2021",
+    },
+    {
+      id: 9,
+      photoName: joffreLakePic,
+      altValue: "Joffre Lake Hike 2022",
+    },
+    {
+      id: 10,
+      photoName: sunshineCoastPic1,
+      altValue: "Sunshine Coast Pre-Departure",
+    },
+    {
+      id: 11,
+      photoName: sunshineCoastPic2,
+      altValue: "Andrea looking down in the valley from Walt Hill",
+    },
+    {
+      id: 12,
+      photoName: icebergLakePic1,
+      altValue: "Iceberg Lake Hike 2022",
+    },
+    {
+      id: 13,
+      photoName: icebergLakePic2,
+      altValue: "Andrea with Canadian flag, Iceberg Lake background",
+    },
+    {
+      id: 14,
+      photoName: mtSproatt2022,
+      altValue: "Mt Sproatt Hike 2022",
     },
   ];
 
