acb0220a5d11deffb68e98e62779fc622aabdc29
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-18 18:45:40 -0800

    Remove commented out code. Move footer tag outside of div with classname container.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import HappyBirthday from "./components/HappyBirthday";
-// import { Carousel } from 'react-bootstrap';
-// import picOne from "./assets/puppies1.webp";
-// import picTwo from "./assets/puppies2.jpeg";
-// import picThree from "./assets/puppies3.webp";
+
 
 function App() {
   return (
@@ -13,40 +10,6 @@ function App() {
         <h1 className="title-text">
           Joyeux 30e Anniversaire, ma ch&#232;re amie!
         </h1>
-
-        {/* <Carousel className="carousel">
-          <Carousel.Item interval={4000}>
-            <img
-              className="d-block w-100"
-              src={picOne}
-              alt="Golden retriever puppies"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={4000}>
-            <img className="d-block w-100" src={picTwo} alt="Aussie puppy" />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={picThree}
-              alt="Daschund mix puppy"
-            />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel> */}
 
         <div className="confetti"></div>
         <div className="confetti"></div>
@@ -92,8 +55,8 @@ function App() {
         <div className="confetti"></div>
         <div className="confetti"></div>
         <HappyBirthday />
-        <footer className="text-center mt-5">Coded by Raiza De Guzman</footer>
       </div>
+      <footer className="text-center">Coded by Raiza De Guzman</footer>
 
     </div>
   );
